acd30ed13728a95fca21808a2f653f6ea9a4dbcd
Jerome <jerome.schmidt@students.makeschool.com>
2021-02-16 16:48:06 -0600

    gets links and titles for website items and determines if they are content, other website links, or ads

--- scrape.go
+++ scrape.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
-
+	"log"
+	"strings"
+	"regexp"
 	"github.com/gocolly/colly"
 )
+
+type StoryArticle struct {
+	Title	string
+	Link	string
+}
+type websiteLinks struct {
+	Title	string
+	Link	string
+}
+type AdLinks struct {
+	Title	string
+	Link	string
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -14,11 +29,40 @@ func main() {
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+		title := e.Text
+        link := e.Attr("href")
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-	})
+		reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		title = reg.ReplaceAllString(title, " ")
+
+		if strings.Contains(link, "https://www.marketwatch.com/story/"){
+			article := websiteLinks{
+				Title:       title,
+				Link:         link,
+			}
+
+			fmt.Printf("Story Link found: %q -> %s\n", article.Title, article.Link)
+
+		} else if strings.Contains(link, "marketwatch.com"){
+			article := StoryArticle{
+				Title:       title,
+				Link:         link,
+			}
+
+			fmt.Printf("Website Link found: %q -> %s\n", article.Title, article.Link)
+		}else {
+				article := AdLinks{
+					Title:       title,
+					Link:         link,
+				}
+
+				fmt.Printf("Ad Link found: %q -> %s\n", article.Title, article.Link)
+		}
+})
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
@@ -26,5 +70,5 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://www.marketwatch.com/")
 }
