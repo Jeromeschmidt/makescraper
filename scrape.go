1f0cd41c742c5733106af70e12d2ecc06661a94d
Jerome <jerome.schmidt@students.makeschool.com>
2021-02-16 17:19:33 -0600

    app now saves json in file

--- scrape.go
+++ scrape.go
@@ -5,21 +5,30 @@ import (
 	"log"
 	"strings"
 	"regexp"
+	"encoding/json"
+	// "io/ioutil"
+	"os"
 	"github.com/gocolly/colly"
 )
 
-type StoryArticle struct {
+type Article struct {
 	Title	string
 	Link	string
+	LinkType string
 }
-type websiteLinks struct {
-	Title	string
-	Link	string
-}
-type AdLinks struct {
-	Title	string
-	Link	string
-}
+
+// type StoryArticle struct {
+// 	Title	string
+// 	Link	string
+// }
+// type websiteLinks struct {
+// 	Title	string
+// 	Link	string
+// }
+// type AdLinks struct {
+// 	Title	string
+// 	Link	string
+// }
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -38,29 +47,54 @@ func main() {
 		}
 
 		title = reg.ReplaceAllString(title, " ")
+		artType := "story"
 
 		if strings.Contains(link, "https://www.marketwatch.com/story/"){
-			article := websiteLinks{
-				Title:       title,
-				Link:         link,
-			}
-
-			fmt.Printf("Story Link found: %q -> %s\n", article.Title, article.Link)
+			// article := websiteLinks{
+			// 	Title:       title,
+			// 	Link:         link,
+			// }
+			//
+			// fmt.Printf("Story Link found: %q -> %s\n", article.Title, article.Link)
+			artType = "story"
 
 		} else if strings.Contains(link, "marketwatch.com"){
-			article := StoryArticle{
-				Title:       title,
-				Link:         link,
-			}
+			// article := StoryArticle{
+			// 	Title:       title,
+			// 	Link:         link,
+			// }
+			//
+			// fmt.Printf("Website Link found: %q -> %s\n", article.Title, article.Link)
+			artType = "other website link"
+		} else {
+				// article := AdLinks{
+				// 	Title:       title,
+				// 	Link:         link,
+				// }
 
-			fmt.Printf("Website Link found: %q -> %s\n", article.Title, article.Link)
-		}else {
-				article := AdLinks{
-					Title:       title,
-					Link:         link,
-				}
+				// fmt.Printf("Ad Link found: %q -> %s\n", article.Title, article.Link)
+				artType = "Ad"
+		}
 
-				fmt.Printf("Ad Link found: %q -> %s\n", article.Title, article.Link)
+		article := Article{
+			Title:       title,
+			Link:         link,
+			LinkType:	artType,
+		}
+
+		JSONarticle, _ := json.MarshalIndent(article, "", " ")
+		fmt.Println(string(JSONarticle))
+		// err = ioutil.WriteFile("output.json", JSONarticle, 0644)
+
+		f, err := os.OpenFile("output.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0660)
+		if err != nil {
+		    panic(err)
+		}
+
+		defer f.Close()
+
+		if _, err = f.WriteString(string(JSONarticle)); err != nil {
+		    panic(err)
 		}
 })
 
